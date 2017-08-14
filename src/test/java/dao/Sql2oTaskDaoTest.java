b38a76f7dcf510e1c97d2ea0e79897269bc2ea8f
Guest User <Guest@Epicodus-6D.local>
2017-08-14 14:32:20 -0700

    add stuff thats cool man

---
+++ src/test/java/dao/Sql2oTaskDaoTest.java
@@ -0,0 +1,30 @@
+package dao;
+
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+/**
+ * Created by Guest on 8/14/17.
+ */
+public class Sql2oTaskDaoTest {
+
+    private Sql2oTaskDao taskDao; //ignore me for now. We'll create this soon.
+    private Connection conn; //must be sql2o class conn
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        taskDao = new Sql2oTaskDao(sql2o); //ignore me for now
+
+        //keep connection open through entire test so it does not get erased.
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+}
